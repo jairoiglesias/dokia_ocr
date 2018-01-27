d9e288156ddbb350465f1ea65311c0e16e85a41b
Jairo Iglesias <jairohighwind@hotmail.com>
2018-01-27 02:08:35 -0200

    Command Line para extrair arquivos individualmente ou de forma coletiva

--- tests/test_tesseract.js
+++ tests/test_tesseract.js
@@ -1,22 +1,69 @@
 
 var tesseract = require('node-tesseract');
+var fs = require('fs')
 
 var options = {
 	l: 'por',
 };
 
-var image = 'file.tiff'
+var arg = process.argv.slice(2);
 
-console.log('Iniciando extração de dados usando Tesseract')
-console.log(image)
+if(arg == '--extract-single-file'){
+    
+    var imageFullPath = 'images/' + arg
+    
+    extractSingleImage(imageFullPath, function(result){
+        console.log(result)
+    })
 
-tesseract.process(image, function(err, text) {
-	if(err) {
-		console.error(err);
-        console.log('Erro ao tentar ler arquivo no Tesseract')
-	} else {
-		console.log(text);
-	}
-})
+}
+else if(arg == '--extract-all-files'){
+
+    var folder = 'images'
+
+    extractAllImages(folder, function(result){
+
+    })
+}
 
 
+function extractSingleImage(imageFullPath, callback){
+
+    tesseract.process(imageFullPath, function(err, text) {
+        if(err) {
+            console.error(err);
+            console.log('Erro ao tentar ler arquivo no Tesseract')
+            callback(err)
+        } else {
+            console.log(text);
+            callback(text)
+        }
+    })
+
+}
+
+function extractAllImages(folder, callback){
+
+    fs.readdir(folder, (err, files) => {
+    
+        files.forEach(file => {
+            
+            if(file.indexOf('tiff') != -1 || file.indexOf('jpeg') != -1 || file.indexOf('jpg') != -1){
+                
+                var imageFullPath = 'images/' + file
+                
+                console.log('Iniciando a extração do arquivo ' + file)
+
+                extractSingleImage(imageFullPath, function(result){
+
+                    fs.writeFile('images/' + file + '.txt', result, function(err) {
+                        if(err) throw err
+
+                        console.log(file + ' => Extração realizada com sucesso!')
+                    })
+                })
+            }
+
+        })
+    })
+}
