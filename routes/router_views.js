f234e200289ed7dc9a24658e9688667fa6ef5687
Jairo Iglesias <jairohighwind@hotmail.com>
2018-01-30 04:18:40 -0200

    Fix server timouet

--- routes/router_views.js
+++ routes/router_views.js
@@ -39,7 +39,7 @@ module.exports = function(app) {
 
   app.post('/upload_doc', upload.any(), (req, res) => {
     
-    req.connection.setTimeout(999999)
+    req.connection.setTimeout(0)
 
     console.log('REQUEST ======================')
     console.log('variavel docSend:')
